8cf5270b7447af53fa10e19a107cfb30e7730b38
JosefGav <101528981+JosefGav@users.noreply.github.com>
2023-11-24 14:23:09 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
+const mode  = document.getElementById("mode")
+let radian = false
+
 class Angle {
     constructor(angle) {
-        angle %= 360
+
+        this.PA = angle % 360
         this.angle = angle
-        console.log("angle")
-        console.log(angle)
 
-        if ( angle == 0 ){ this.opp = "0"; this.adj = "1"; this.hyp = "1" }
-        else if ( angle == 90 ) { this.opp = "1"; this.adj = "0"; this.hyp = "1"; this.quadrant = undefined}
-        else if ( angle == 180 ) { this.opp = "0"; this.adj = "-1"; this.hyp = "1"; this.quadrant = undefined }
-        else if ( angle == 270 ) { this.opp = "-1"; this.adj = "0"; this.hyp = "1"; this.quadrant = undefined }
+        if ( this.PA == 0 ){ this.opp = "0"; this.adj = "1"; this.hyp = "1" }
+        else if ( this.PA == 90 ) { this.opp = "1"; this.adj = "0"; this.hyp = "1"; this.quadrant = undefined}
+        else if ( this.PA == 180 ) { this.opp = "0"; this.adj = "-1"; this.hyp = "1"; this.quadrant = undefined }
+        else if ( this.PA == 270 ) { this.opp = "-1"; this.adj = "0"; this.hyp = "1"; this.quadrant = undefined }
 
-        else if      (0 < angle && angle  < 90)   { this.quadrant = 1; this.RA = angle; }
-        else if (90 < angle && angle < 180)  { this.quadrant = 2; this.RA = 180 - angle; }
-        else if (180 < angle && angle < 270) { this.quadrant = 3; this.RA = angle - 180; }
-        else if (270 < angle && angle < 360) { this.quadrant = 4; this.RA = 360 - angle; }
+        else if      (0 < this.PA && this.PA  < 90)   { this.quadrant = 1; this.RA = this.PA; }
+        else if (90 < this.PA && this.PA < 180)  { this.quadrant = 2; this.RA = 180 - this.PA; }
+        else if (180 < this.PA && this.PA < 270) { this.quadrant = 3; this.RA = this.PA - 180; }
+        else if (270 < this.PA && this.PA < 360) { this.quadrant = 4; this.RA = 360 - this.PA; }
 
         
         
-        if      (this.RA == 30) { this.opp = "1"; this.adj = "\\sqrt{3}"; this.hyp = "2" }
-        else if (this.RA == 60) { this.opp = "\\sqrt{3}"; this.adj = "1"; this.hyp = "2" }
-        else if (this.RA == 45) { this.opp = "1"; this.adj = "1"; this.hyp = "\\sqrt{2}" }
+        if (this.RA == 30) { 
+            this.opp = "1"; 
+            this.adj = "\\sqrt{3}"; 
+            this.hyp = "2";
+            this.rad = [this.PA/this.RA,6];
+        }
+        else if (this.RA == 60) { 
+            this.opp = "\\sqrt{3}"; 
+            this.adj = "1"; 
+            this.hyp = "2" 
+            this.rad = [this.PA/this.RA,3];
+        }
+        else if (this.RA == 45) { 
+            this.opp = "1"; 
+            this.adj = "1"; 
+            this.hyp = "\\sqrt{2}" 
+            this.rad = [this.PA/this.RA,4];
+        }
+        this.radTEX = "$$\\frac{"+this.rad[0].toString()+"\\pi}{"+ this.rad[1].toString()+"}$$"
     }
 
     sin() {
@@ -69,7 +87,7 @@ class Angle {
         return string;
     }
 }
-function regenerate() {
+function regenerate(rad) {
     let angleBase = Math.floor(Math.random() * 2); // 0-1, rep 30-45
     let multiplier = Math.floor(Math.random() * 12) + 1;  // 1-12
 
@@ -78,20 +96,40 @@ function regenerate() {
     if (angleBase == 0) base = 30;
     if (angleBase == 1) base = 45;
     let theta = new Angle(base*multiplier);
-    // document.getElementById("top").innerHTML = angleBase*multiplier
-    // document.getElementById("type").innerHTML = angleBase
-    document.getElementById("frontSin").innerHTML = "sin"+theta.angle.toString()
+
+    document.getElementById("frontSin").innerHTML = "sin" + theta.radTEX //"sin"+theta.angle.toString()+"°"
     document.getElementById("backSin").innerHTML =  theta.sin()
 
-    document.getElementById("frontCos").innerHTML = "cos"+theta.angle.toString()
+    document.getElementById("frontCos").innerHTML = "cos" + theta.radTEX//"cos"+theta.angle.toString() + "°"
     document.getElementById("backCos").innerHTML =  theta.cos()
 
-    document.getElementById("frontTan").innerHTML = "tan"+theta.angle.toString()
-    document.getElementById("backTan").innerHTML =  theta.tan()
+    document.getElementById("frontTan").innerHTML = "tan" + theta.radTEX//"tan"+theta.angle.toString() + "°"
+    document.getElementById("backTan").innerHTML =  theta.tan() 
+
+    if (rad) {
+        document.getElementById("frontSin").innerHTML = "sin" + theta.radTEX
+        document.getElementById("frontCos").innerHTML = "cos" + theta.radTEX
+        document.getElementById("frontTan").innerHTML = "tan" + theta.radTEX
+    } else {
+        document.getElementById("frontSin").innerHTML = "sin"+theta.angle.toString()+"°"
+        document.getElementById("frontCos").innerHTML = "cos"+theta.angle.toString() + "°"
+        document.getElementById("frontTan").innerHTML = "tan"+theta.angle.toString() + "°"
+    }
 
 }
 
 document.getElementById("regenerate").addEventListener("click", function() {
-    regenerate()
+    regenerate(radian)
     MathJax.typesetPromise()
+})
+
+mode.addEventListener("click", function() {
+    if (radian) {
+        radian = false
+        mode.innerHTML = "Degree"
+    } else {
+        radian = true
+        mode.innerHTML = "Radians"
+    }
+
 })
